7b980e8ebd4da4296ec27618f0cc7fe62a4399dd
Abdulmajid Awol <maajidawol@gmail.com>
2024-08-21 09:30:19 +0300

    about to change

--- image_app/lib/data/data_sources/remote_data_source.dart
+++ image_app/lib/data/data_sources/remote_data_source.dart
@@ -29,6 +29,26 @@ class RemoteDataSource {
       return [];
     }
   }
+  Future<String> chat({required String prompt}) async {
+    String url =
+        'https://real-time-image-search.p.rapidapi.com/search?query=${term}&limit=10&size=any&color=any&type=any&time=any&usage_rights=any&file_type=any&aspect_ratio=any&safe_search=off&region=us';
+    final result = await client.get(Uri.parse(url), headers: {
+      "x-rapidapi-host": "real-time-image-search.p.rapidapi.com",
+      "x-rapidapi-key": "729c2a12d6mshe0e7274c186125dp12f0c6jsn8cfa728d6a12"
+    });
+    if (result.statusCode == 200) {
+      
+      final resultList = json.decode(result.body)["data"];
+
+      List<ImageModel> images =
+          (resultList as List).map((img) => ImageModel.fromJson(img)).toList();
+ 
+      return images;
+    } else {
+      return [];
+    }
+  }
+
 }
 
 
